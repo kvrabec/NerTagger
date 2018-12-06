2660df430a76c050d639caff781758a0255a2646
Kristijan Vrabec <kiki95173@gmail.com>
2018-12-06 19:43:18 +0100

    Multiple models trained by parametars defined in modelParametars.json with graphs

--- NerTagger/main.py
+++ NerTagger/main.py
@@ -1,21 +1,22 @@
 import os
 import sys
 import model
+import graph
 from sklearn.model_selection import train_test_split
-from prepareEmbeddings import (prepare_text_for_prediction,X_word, X_char,
+from prepareEmbeddings import (prepare_text_for_prediction,X_word, X_char,y,
                      n_words, n_tags, n_chars, max_len, max_len_char,
                      word2idx, idx2word, tag2idx, idx2tag, char2idx)
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="4"
+model_name = os.path.dirname(os.path.abspath(__file__)) + '\\model1'
 model_file_name = os.path.dirname(os.path.abspath(__file__)) + '\\model1.h5'
-
-print(model_file_name)
 
 if not os.path.exists(model_file_name) or not os.path.isfile(model_file_name):
     X_word_tr, X_word_te, y_tr, y_te = train_test_split(X_word, y, test_size=0.1, random_state=2018)
     X_char_tr, X_char_te, _, _ = train_test_split(X_char, y, test_size=0.1, random_state=2018)
-    model.train(X_word_tr, y_tr, X_char_tr, n_words, n_tags, n_chars, max_len, max_len_char, model_file_name)
+    model.train(X_word_tr, y_tr, X_char_tr, n_words, n_tags, n_chars, max_len, max_len_char, model_name)
     model.predict(X_word_te, X_char_te, y_te, max_len, max_len_char, idx2word, idx2tag, model_file_name)
+    graph.draw_training_graph(model_name + '.csv')
 
 if len(sys.argv) < 2:
     raise AttributeError("Enter text for tagging!")
